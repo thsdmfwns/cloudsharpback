cd0422092b530ca8086df2fd97132a751248bdf0
son <son@SONLAPTOP>
2023-01-02 18:31:30 +0900

    Add AuthControllerBase

---
+++ cloudsharpback/Controllers/Base/AuthControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using cloudsharpback.Attribute;
+using cloudsharpback.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cloudsharpback.Controllers.Base
+{
+    [Auth]
+    public class AuthControllerBase : ControllerBase
+    {
+        public MemberDto? MemberDto => HttpContext.Items["member"] as MemberDto;
+    }
+}
