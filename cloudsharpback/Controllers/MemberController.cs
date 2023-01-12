e2a9feef46bb86ef4dabe57ef178d4becaa3eea3
son <son@SONLAPTOP>
2023-01-12 19:20:56 +0900

    migration controllers

--- cloudsharpback/Controllers/MemberController.cs
+++ cloudsharpback/Controllers/MemberController.cs
@@ -13,14 +13,12 @@ namespace cloudsharpback.Controllers
     public class MemberController : AuthControllerBase
     {
         private readonly IJWTService jwtService;
-        private readonly IMemberService userService;
-        private readonly IFileService fileService;
+        private readonly IMemberService memberService;
 
-        public MemberController(IJWTService jwtService, IMemberService userService, IFileService fileService)
+        public MemberController(IJWTService jwtService, IMemberService memberService)
         {
             this.jwtService = jwtService;
-            this.userService = userService;
-            this.fileService = fileService;
+            this.memberService = memberService;
         }
 
         [AllowAnonymous]
@@ -32,7 +30,7 @@ namespace cloudsharpback.Controllers
             {
                 return StatusCode(403, "bad token");
             }
-            var result = await userService.GetMemberById(memberId.Value);
+            var result = await memberService.GetMemberById(memberId.Value);
             if (result.err is not null || result.result is null)
             {
                 return StatusCode(result.err!.ErrorCode, result.err.Message);
@@ -40,40 +38,6 @@ namespace cloudsharpback.Controllers
             var acToken = jwtService.WriteAcessToken(result.result);
             return Ok(acToken);
         }
-
-        [AllowAnonymous]
-        [HttpPost("login")]
-        public async Task<IActionResult> Login(LoginDto loginDto)
-        {
-            var result = await userService.Login(loginDto);
-            if (result.err is not null || result.result is null)
-            {
-                return StatusCode(result.err!.ErrorCode, result.err.Message);
-            }
-            var res = new TokenDto(jwtService.WriteAcessToken(result.result), jwtService.WriteRefeshToken(result.result));
-            return Ok(res);
-        }
-
-        [AllowAnonymous]
-        [HttpPost("register")]
-        public async Task<IActionResult> Register(RegisterDto registerDto)
-        {
-            var res = await userService.Register(registerDto, 2);
-            if (res.err is not null || res.directoryId is null)
-            {
-                return StatusCode(res.err!.ErrorCode, res.err.Message);
-            }
-            fileService.MakeTemplateDirectory(res.directoryId);
-            return Ok();
-        }
-
-        [AllowAnonymous]
-        [HttpGet("idcheck")]
-        public async Task<IActionResult> IdCkeck(string id)
-        {
-            return Ok(await userService.IdCheck(id));
-        }
-
 
         [HttpGet("member")]
         public IActionResult GetMember()
@@ -84,7 +48,7 @@ namespace cloudsharpback.Controllers
         [HttpPost("updateImage")]
         public async Task<IActionResult> UploadProfileImage(IFormFile image)
         {
-            var res = await userService.UploadProfileImage(image, Member);
+            var res = await memberService.UploadProfileImage(image, Member);
             if (res is not null)
             {
                 return StatusCode(res.ErrorCode, res.Message);
@@ -96,7 +60,7 @@ namespace cloudsharpback.Controllers
         [HttpGet("imageDL/{image}")]
         public IActionResult DownloadProfileImage(string image)
         {
-            var err = userService.DownloadProfileImage(image, out var fileStream, out var contentType);
+            var err = memberService.DownloadProfileImage(image, out var fileStream, out var contentType);
             if (err is not null || fileStream is null || contentType is null)
             {
                 return StatusCode(err!.ErrorCode, err.Message);
@@ -112,7 +76,7 @@ namespace cloudsharpback.Controllers
         [HttpPost("updateNick")]
         public async Task<IActionResult> UpdateNickName(string nickname)
         {
-            var err = await userService.UpadteNickname(Member, nickname);
+            var err = await memberService.UpadteNickname(Member, nickname);
             if (err is not null)
             {
                 return StatusCode(err.ErrorCode, err.Message);
@@ -123,7 +87,7 @@ namespace cloudsharpback.Controllers
         [HttpPost("updateEmail")]
         public async Task<IActionResult> UpdateEmail(string email)
         {
-            var err = await userService.UpadteEmail(Member, email);
+            var err = await memberService.UpadteEmail(Member, email);
             if (err is not null)
             {
                 return StatusCode(err.ErrorCode, err.Message);
@@ -134,7 +98,7 @@ namespace cloudsharpback.Controllers
         [HttpPost("checkPw")]
         public async Task<IActionResult> CheckPassword([FromBody]string password)
         {
-            var result = await userService.CheckPassword(Member, password);
+            var result = await memberService.CheckPassword(Member, password);
             if (result.err is not null)
             {
                 return StatusCode(result.err.ErrorCode, result.err.Message);
@@ -145,7 +109,7 @@ namespace cloudsharpback.Controllers
         [HttpPost("updatePw")]
         public async Task<IActionResult> UpadtePassword(UpadtePasswordDto requset)
         {
-            var err = await userService.UpdatePassword(Member, requset);
+            var err = await memberService.UpdatePassword(Member, requset);
             if (err is not null)
             {
                 return StatusCode(err.ErrorCode, err.Message);
---
+++ cloudsharpback/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using cloudsharpback.Models;
+using cloudsharpback.Services;
+using cloudsharpback.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cloudsharpback.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserService userService;
+        private readonly IJWTService jwtService;
+        private readonly IFileService fileService;
+
+
+        public UserController(IUserService userService, IJWTService jwtService, IFileService fileService)
+        {
+            this.userService = userService;
+            this.jwtService = jwtService;
+            this.fileService = fileService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            var result = await userService.Login(loginDto);
+            if (result.err is not null || result.result is null)
+            {
+                return StatusCode(result.err!.ErrorCode, result.err.Message);
+            }
+            var res = new TokenDto(jwtService.WriteAcessToken(result.result), jwtService.WriteRefeshToken(result.result));
+            return Ok(res);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            var res = await userService.Register(registerDto, 2);
+            if (res.err is not null || res.directoryId is null)
+            {
+                return StatusCode(res.err!.ErrorCode, res.err.Message);
+            }
+            fileService.MakeTemplateDirectory(res.directoryId);
+            return Ok();
+        }
+
+        [AllowAnonymous]
+        [HttpGet("idcheck")]
+        public async Task<IActionResult> IdCkeck(string id)
+        {
+            return Ok(await userService.IdCheck(id));
+        }
+    }
+}
