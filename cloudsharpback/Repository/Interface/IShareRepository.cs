2b4c41c4a1472b0744a2e5841f12094dca41e407
son <son2044@gmail.com>
2023-06-09 17:21:34 +0900

    make ShareRepository.cs

---
+++ cloudsharpback/Repository/Interface/IShareRepository.cs
@@ -0,0 +1,15 @@
+using cloudsharpback.Models;
+
+namespace cloudsharpback.Repository.Interface;
+
+public interface IShareRepository
+{
+    Task<bool> TryAddShare(ulong memberId, ShareRequestDto req, string password, FileInfo fileinfo);
+    Task<ShareResponseDto?> GetShareByToken(string token);
+    Task<List<ShareResponseDto>> GetSharesListByMemberId(ulong memberId);
+    Task<ShareDownloadDto?> GetShareDownloadDtoByToken(string token);
+    Task<bool> TrySetShareExpireTimeToZero(ulong memberId, string token);
+    Task<bool> TryUpdateShare(ulong memberId, string token, ShareUpdateDto dto, string password);
+    Task<bool> TryDeleteShare(ulong memberId, string targetFilePath);
+    Task<string?> GetPasswordHashByToken(string token);
+}
