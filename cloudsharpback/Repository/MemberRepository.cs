dd41ba469b5399d65c35a6a0d830adfcf0890eef
son <son2044@gmail.com>
2023-06-09 17:21:17 +0900

    Make sql string to const

--- cloudsharpback/Repository/MemberRepository.cs
+++ cloudsharpback/Repository/MemberRepository.cs
@@ -16,29 +16,29 @@ public class MemberRepository : IMemberRepository
 
     public async Task<MemberDto?> GetMemberById(ulong id)
     {
-        var query = "SELECT member_id id, role_id role, email, nickname, " +
+        const string sql = "SELECT member_id id, role_id role, email, nickname, " +
                     "BIN_TO_UUID(directory) directory, profile_image profileImage " +
                     "FROM member " +
                     "WHERE member_id = @Id";
         using var conn = _connService.Connection;
-        return await conn.QuerySingleOrDefaultAsync<MemberDto>(query, new { Id = id });
+        return await conn.QuerySingleOrDefaultAsync<MemberDto>(sql, new { Id = id });
     }
     
     public async Task<MemberDto?> GetMemberByLoginId(string id)
     {
-        var query = "SELECT member_id id, role_id role, email, nickname, " +
+        const string sql = "SELECT member_id id, role_id role, email, nickname, " +
                     "BIN_TO_UUID(directory) directory, profile_image profileImage " +
                     "FROM member " +
                     "WHERE id = @Id";
         using var conn = _connService.Connection;
-        return await conn.QuerySingleOrDefaultAsync<MemberDto>(query, new { Id = id });
+        return await conn.QuerySingleOrDefaultAsync<MemberDto>(sql, new { Id = id });
     }
 
     public async Task<bool> TryUpdateMemberProfileImage(ulong id, string imageFileName)
     {
-        var sql = "UPDATE member " +
-                  "SET profile_image = @Filename " +
-                  "WHERE member_id = @Id";
+        const string sql = "UPDATE member " +
+                           "SET profile_image = @Filename " +
+                           "WHERE member_id = @Id";
         using var conn = _connService.Connection;
         var result = await conn.ExecuteAsync(sql, new
         {
@@ -50,9 +50,9 @@ public class MemberRepository : IMemberRepository
 
     public async Task<bool> TryUpdateMemberNickname(ulong id, string nickname)
     {
-        var sql = "UPDATE member " +
-                  "SET nickname = @ChangeNick " +
-                  "WHERE member_id = @Id";
+        const string sql = "UPDATE member " +
+                           "SET nickname = @ChangeNick " +
+                           "WHERE member_id = @Id";
         using var conn = _connService.Connection;
         var result = await conn.ExecuteAsync(sql, new
         {
@@ -64,9 +64,9 @@ public class MemberRepository : IMemberRepository
     
     public async Task<bool> TryUpdateMemberEmail(ulong id, string email)
     {
-        var sql = "UPDATE member " +
-                  "SET email = @Email " +
-                  "WHERE member_id = @Id";
+        const string sql = "UPDATE member " +
+                     "SET email = @Email " +
+                     "WHERE member_id = @Id";
         using var conn = _connService.Connection;
         var result = await conn.ExecuteAsync(sql, new
         {
@@ -78,28 +78,28 @@ public class MemberRepository : IMemberRepository
 
     public async Task<string?> GetMemberPasswordHashById(ulong id)
     {
-        var sql = "SELECT password FROM member WHERE member_id = @Id";
+        const string sql = "SELECT password FROM member WHERE member_id = @Id";
         using var conn = _connService.Connection;
         return await conn.QuerySingleOrDefaultAsync<string?>(sql, new { Id = id });
     }
     
     public async Task<string?> GetMemberPasswordHashByLoginId(string id)
     {
-        var sql = "SELECT password FROM member WHERE id = @Id";
+        const string sql = "SELECT password FROM member WHERE id = @Id";
         using var conn = _connService.Connection;
         return await conn.QuerySingleOrDefaultAsync<string?>(sql, new { Id = id });
     }
     
     public async Task<bool> TryLoginIdDuplicate(string id)
     {
-        var sql = "SELECT member_id FROM member WHERE id = @id";
+        const string sql = "SELECT member_id FROM member WHERE id = @id";
         using var conn = _connService.Connection;
         return (await conn.QueryAsync(sql, new { Id = id })).Any();
     }
     
     public async Task<bool> TryUpdateMemberPassword(ulong id, string password)
     {
-        var sql = "UPDATE member SET password = @Password WHERE member_id = @Id";
+        const string sql = "UPDATE member SET password = @Password WHERE member_id = @Id";
         using var conn = _connService.Connection;
         var result = await conn.ExecuteAsync(sql, new { Password = password, Id = id});
         return result > 0;
@@ -108,9 +108,9 @@ public class MemberRepository : IMemberRepository
     public async Task<bool> TryAddMember(RegisterDto registerDto, ulong role)
     {
         using var conn = _connService.Connection;
-        var query = "INSERT INTO member(id, password, nickname, role_id, email, directory) " +
+        const string sql = "INSERT INTO member(id, password, nickname, role_id, email, directory) " +
                     "VALUES(@Id, @Pw, @Nick, @Role, @Email, UUID_TO_BIN(@Directory))";
-        var result = await conn.ExecuteAsync(query, new
+        var result = await conn.ExecuteAsync(sql, new
         {
             Id = registerDto.Id,
             Pw = registerDto.Pw,
