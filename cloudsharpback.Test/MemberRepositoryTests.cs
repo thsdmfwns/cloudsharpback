4451255a39c00c03a8ef914808d97a7429fe0811
son <son2044@gmail.com>
2023-07-31 13:58:54 +0900

    Set TestSetup

--- cloudsharpback.Test/MemberRepositoryTests.cs
+++ cloudsharpback.Test/MemberRepositoryTests.cs
@@ -1,223 +1,58 @@
+using Bogus;
 using cloudsharpback.Repository;
 using cloudsharpback.Repository.Interface;
 using cloudsharpback.Utills;
+using Dapper;
 
 namespace cloudsharpback.Test;
 
 public class MemberRepositoryTests
 {
-    private MemberRepository _repository => new MemberRepository(DBConnectionFactoryMock.Mock.Object);
+    protected record Member(ulong MemberId, string Id, string Password, string Nick, ulong Role, string Email,
+        string Dir);
+
+    private MemberRepository _repository;
+    private List<Member> _members = new List<Member>();
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
-        
-    }
-
-    [Test]
-    public async Task GetMemberById()
-    {
-        var idsucs = new List<ulong>();
-        idsucs.Add(33);
-        idsucs.Add(34);
-        var idfails = new List<ulong>();
-        idfails.Add(32);
-        idfails.Add(35);
-
-        var repo = _repository;
-        foreach (var x in idsucs)
+        _repository = new MemberRepository(DBConnectionFactoryMock.Mock.Object);
+        _members = new List<Member>();
+        var insertSql = @"
+INSERT INTO member
+VALUES (@memberId, @id, @password, @nick, @role, @email, @dir, null);
+";
+        ulong memberid = 1;
+        var faker = new Faker();
+        for (int i = 0; i < 2; i++)
         {
-            var res = await repo.GetMemberById(x);
-            Assert.That(res, Is.Not.Null);
-            Assert.That(res!.Id, Is.EqualTo(x));
+            var mem = new Member(
+                memberid++,
+                faker.Internet.UserName(),
+                faker.Internet.Password(),
+                faker.Internet.UserName(),
+                2,
+                faker.Internet.Email(),
+                Guid.NewGuid().ToString()
+                );
+            _members.Add(mem);
         }
-        foreach (var x in idfails)
+        using var conn = DBConnectionFactoryMock.Mock.Object.Connection;
+        await conn.ExecuteAsync("DELETE FROM member");
+        foreach (var mem in _members)
         {
-            Assert.That(await repo.GetMemberById(x), Is.Null);
-        }
-    }
-    
-    [Test]
-    public async Task GetMemberByLoginId()
-    {
-        var idsucs = new List<(string, int)>();
-        idsucs.Add(("test", 33));
-        idsucs.Add(("string", 34));
-        var idfails = new List<string>();
-        idfails.Add("asd");
-        idfails.Add("qwe");
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            var res = await repo.GetMemberByLoginId(x.Item1);
-            Assert.That(res, Is.Not.Null);
-            Assert.That(res!.Id, Is.EqualTo(x.Item2));
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.GetMemberByLoginId(x), Is.Null);
+            await conn.ExecuteAsync(insertSql, new
+            {
+                memberId = mem.MemberId,
+                id = mem.Id,
+                password = PasswordEncrypt.EncryptPassword(mem.Password),
+                nick = mem.Nick,
+                email = mem.Email,
+                dir = mem.Dir
+            });  
         }
     }
     
-    [Test]
-    public async Task TryUpdateMemberProfileImage()
-    {
-        var idsucs = new List<(ulong , string)>();
-        idsucs.Add((33, "test1"));
-        idsucs.Add((34, "test2"));
-        var idfails = new List<(ulong , string)>();
-        idfails.Add((32, "test3"));
-        idfails.Add((35, "test4"));
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            Assert.That(await repo.TryUpdateMemberProfileImage(x.Item1, x.Item2), Is.True);
-            var res = await repo.GetMemberById(x.Item1);
-            Assert.That(res!.ProfileImage, Is.EqualTo(x.Item2));
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.TryUpdateMemberProfileImage(x.Item1, x.Item2), Is.False);
-        }
-    }
     
-    [Test]
-    public async Task TryUpdateMemberNickname()
-    {
-        var idsucs = new List<(ulong , string)>();
-        idsucs.Add((33, "test1"));
-        idsucs.Add((34, "test2"));
-        var idfails = new List<(ulong , string)>();
-        idfails.Add((32, "test3"));
-        idfails.Add((35, "test4"));
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            Assert.That(await repo.TryUpdateMemberNickname(x.Item1, x.Item2), Is.True);
-            var res = await repo.GetMemberById(x.Item1);
-            Assert.That(res!.Nickname, Is.EqualTo(x.Item2));
-
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.TryUpdateMemberNickname(x.Item1, x.Item2), Is.False);
-        }
-    }
-    
-    [Test]
-    public async Task TryUpdateMemberEmail()
-    {
-        var idsucs = new List<(ulong , string)>();
-        idsucs.Add((33, "test1"));
-        idsucs.Add((34, "test2"));
-        var idfails = new List<(ulong , string)>();
-        idfails.Add((32, "test3"));
-        idfails.Add((35, "test4"));
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            Assert.That(await repo.TryUpdateMemberEmail(x.Item1, x.Item2), Is.True);
-            var res = await repo.GetMemberById(x.Item1);
-            Assert.That(res!.Email, Is.EqualTo(x.Item2));
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.TryUpdateMemberEmail(x.Item1, x.Item2), Is.False);
-        }
-    }
-    
-    [Test]
-    public async Task GetMemberPasswordHashById()
-    {
-        var idsucs = new List<(ulong, string)>();
-        idsucs.Add((33, "test"));
-        idsucs.Add((34, "string"));
-        var idfails = new List<ulong>();
-        idfails.Add(32);
-        idfails.Add(35);
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            var res = await repo.GetMemberPasswordHashById(x.Item1);
-            Assert.That(res, Is.Not.Null);
-            Assert.That(PasswordEncrypt.VerifyPassword(x.Item2, res!), Is.True);
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.GetMemberPasswordHashById(x), Is.Null);
-        }
-    }
-    
-    [Test]
-    public async Task GetMemberPasswordHashByLoginId()
-    {
-        var idsucs = new List<(string, string)>();
-        idsucs.Add(("test", "test"));
-        idsucs.Add(("string", "string"));
-        var idfails = new List<string>();
-        idfails.Add("asd");
-        idfails.Add("qwe");
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            var res = await repo.GetMemberPasswordHashByLoginId(x.Item1);
-            Assert.That(res, Is.Not.Null);
-            Assert.That(PasswordEncrypt.VerifyPassword(x.Item2, res!), Is.True);
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.GetMemberPasswordHashByLoginId(x), Is.Null);
-        }
-    }
-    
-    [Test]
-    public async Task TryLoginIdDuplicate()
-    {
-        var idsucs = new List<string>();
-        idsucs.Add("test");
-        idsucs.Add("string");
-        var idfails = new List<string>();
-        idfails.Add("asd");
-        idfails.Add("qwe");
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            Assert.That(await repo.TryLoginIdDuplicate(x), Is.True);
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.TryLoginIdDuplicate(x), Is.False);
-        }
-    }
-    
-    [Test]
-    public async Task TryUpdateMemberPassword()
-    {
-        var idsucs = new List<(string, ulong)>();
-        idsucs.Add(("test", 33));
-        idsucs.Add(("string", 34));
-        var idfails = new List<(string, ulong)>();
-        idfails.Add(("asd", 35));
-        idfails.Add(("qwe", 32));
-
-        var repo = _repository;
-        foreach (var x in idsucs)
-        {
-            Assert.That(await repo.TryUpdateMemberPassword(x.Item2, PasswordEncrypt.EncryptPassword(x.Item1)), Is.True);
-            var res = await repo.GetMemberPasswordHashById(x.Item2);
-            Assert.That(PasswordEncrypt.VerifyPassword(x.Item1, res!), Is.True);
-
-        }
-        foreach (var x in idfails)
-        {
-            Assert.That(await repo.TryUpdateMemberPassword(x.Item2, x.Item1), Is.False);
-        }
-    }
     
 }
