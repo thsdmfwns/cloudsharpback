d465769043369b751c543fa8259f98328cd67be9
son <son2044@gmail.com>
2023-11-08 10:50:35 +0900

    add project

---
+++ cloudsharpback.E2ETest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace cloudsharpback.E2ETest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
